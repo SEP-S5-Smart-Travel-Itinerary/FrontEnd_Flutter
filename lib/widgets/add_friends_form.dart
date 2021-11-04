f3e4217fcbafc0f0c20dede48116d2dd1fb316c0
malithi-lalanika <66944661+malithi-lalanika@users.noreply.github.com>
2021-11-04 10:38:45 +0530

    valid

--- lib/widgets/add_friends_form.dart
+++ lib/widgets/add_friends_form.dart
@@ -20,6 +20,7 @@ class _AddFriendsDialogState extends State<AddFriendsDialog> {
   bool result = false;
   bool result2 = false;
   String touser = "";
+  GlobalKey<FormState> _formkey1 = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -31,132 +32,146 @@ class _AddFriendsDialogState extends State<AddFriendsDialog> {
         ),
         body: SingleChildScrollView(
           padding: const EdgeInsets.symmetric(horizontal: 30.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              const SizedBox(height: 30.0),
-
-              Text(
-                'Add Trip Mates',
-                style: TextStyle(color: PrimaryColor),
-              ),
-              SizedBox(height: 5),
-
-              // Add countries
-              TextFormField(
-                cursorColor: PrimaryColor,
-                decoration: const InputDecoration(
-                  prefixIcon: Icon(Icons.account_balance_wallet_outlined),
-                  filled: true,
-                  fillColor: SecondayColorBlue,
-                  labelText: 'Add Trip Mates',
-                  labelStyle: TextStyle(color: PrimaryColor),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(5),
-                    ),
-                    borderSide: BorderSide(color: PrimaryColor, width: 2),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(5),
-                    ),
-                    borderSide: BorderSide(color: PrimaryColor, width: 2),
-                  ),
+          child: Form(
+            key: _formkey1,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                const SizedBox(height: 30.0),
+          
+                Text(
+                  'Add Trip Mates',
+                  style: TextStyle(color: PrimaryColor),
                 ),
-                onChanged: (String? value) {
-                  setState(() {
-                    this._name = value;
-                  });
-                },
-
-                // validator: _validateName,
-              ),
-
-              SizedBox(
-                height: 30,
-              ),
-
-              ElevatedButton(
-                onPressed: () {
-                  _friends.add(_name!);
-                  print(_friends);
-                  print(widget.text);
-                  //Addfriends(_friends);
-                  userSearch(_name!).then((value) => {
-                        if (result)
-                          {
-                            memberSearch(_name!).then((value) => {
-                                  if (result2)
-                                    {
-                                      Addfriends(_friends),
-                                      //touserText("Successfully Added").then((value) =>
-                                      Navigator.pushReplacement(
-                                          context,
-                                          new MaterialPageRoute(
-                                              builder: (context) =>
-                                                  AddFriendsDialog(
-                                                      text:
-                                                          "Successfully Added")))
-                                    }
-                                  else
-                                    {
-                                      Navigator.pushReplacement(
-                                          context,
-                                          new MaterialPageRoute(
-                                              builder: (context) =>
-                                                  AddFriendsDialog(
-                                                      text:
-                                                          "Input user is alreay exist in the plan")))
-                                    }
-                                })
-                          }
-                        else
-                          {
-                            //touserText("Input user haven't Registerd Star Trips").then((value) =>
-                            Navigator.pushReplacement(
-                                context,
-                                new MaterialPageRoute(
-                                    builder: (context) => AddFriendsDialog(
-                                        text:
-                                            "Input user haven't Registerd Star Trips")))
-                          }
-                      });
-                  //print(result);
-                },
-                child: Container(
-                    height: 50,
-                    child: Center(
-                        child: const Text(
-                      'Add Trip Mates',
-                      style: TextStyle(fontSize: 18),
-                    ))),
-                style: ElevatedButton.styleFrom(
-                  primary: PrimaryColor,
-                ),
-              ),
-              TextButton(
-                  onPressed: () {
-                    getPalnDetails().then((value) => Navigator.pushReplacement(
-                        context,
-                        new MaterialPageRoute(
-                            builder: (context) => TravelPlanView())));
-                    //             Navigator.push(
-                    // context, new MaterialPageRoute(builder: (context) => TravelPlanView()));
+                SizedBox(height: 5),
+          
+                // Add countries
+                TextFormField(
+                  cursorColor: PrimaryColor,
+                  decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.account_balance_wallet_outlined),
+                    filled: true,
+                    fillColor: SecondayColorBlue,
+                    labelText: 'Add Trip Mates',
+                    labelStyle: TextStyle(color: PrimaryColor),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(5),
+                      ),
+                      borderSide: BorderSide(color: PrimaryColor, width: 2),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(5),
+                      ),
+                      borderSide: BorderSide(color: PrimaryColor, width: 2),
+                    ),
+                  ),
+                  validator: (_val) {
+                    if (_val == "") {
+                      return "Email can't be Empty";
+                    } else {
+                      return null;
+                    }
                   },
-                  child: Text(
-                    'Go to the Plan',
-                    style: TextStyle(color: PrimaryColor),
-                  )),
-
-              Text(
-                widget.text,
-                style: TextStyle(
-                    color: Colors.purple, fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center,
-              ),
-            ],
+                  onChanged: (String? value) {
+                    setState(() {
+                      this._name = value;
+                    });
+                  },
+          
+                  // validator: _validateName,
+                ),
+          
+                SizedBox(
+                  height: 30,
+                ),
+          
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formkey1.currentState!.validate()) {
+                    _friends.add(_name!);
+                    print(_friends);
+                    print(widget.text);
+                    //Addfriends(_friends);
+                    userSearch(_name!).then((value) => {
+                          if (result)
+                            {
+                              memberSearch(_name!).then((value) => {
+                                    if (result2)
+                                      {
+                                        Addfriends(_friends),
+                                        //touserText("Successfully Added").then((value) =>
+                                        Navigator.pushReplacement(
+                                            context,
+                                            new MaterialPageRoute(
+                                                builder: (context) =>
+                                                    AddFriendsDialog(
+                                                        text:
+                                                            "Successfully Added")))
+                                      }
+                                    else
+                                      {
+                                        Navigator.pushReplacement(
+                                            context,
+                                            new MaterialPageRoute(
+                                                builder: (context) =>
+                                                    AddFriendsDialog(
+                                                        text:
+                                                            "Input user is alreay exist in the plan")))
+                                      }
+                                  })
+                            }
+                          else
+                            {
+                              //touserText("Input user haven't Registerd Star Trips").then((value) =>
+                              Navigator.pushReplacement(
+                                  context,
+                                  new MaterialPageRoute(
+                                      builder: (context) => AddFriendsDialog(
+                                          text:
+                                              "Input user haven't Registerd Star Trips")))
+                            }
+                        });
+                        } else {
+                      print("not ok");
+                    }
+                    //print(result);
+                  },
+                  child: Container(
+                      height: 50,
+                      child: Center(
+                          child: const Text(
+                        'Add Trip Mates',
+                        style: TextStyle(fontSize: 18),
+                      ))),
+                  style: ElevatedButton.styleFrom(
+                    primary: PrimaryColor,
+                  ),
+                ),
+                TextButton(
+                    onPressed: () {
+                      getPalnDetails().then((value) => Navigator.pushReplacement(
+                          context,
+                          new MaterialPageRoute(
+                              builder: (context) => TravelPlanView())));
+                      //             Navigator.push(
+                      // context, new MaterialPageRoute(builder: (context) => TravelPlanView()));
+                    },
+                    child: Text(
+                      'Go to the Plan',
+                      style: TextStyle(color: PrimaryColor),
+                    )),
+          
+                Text(
+                  widget.text,
+                  style: TextStyle(
+                      color: Colors.purple, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
           ),
         ));
   }
